03f89a37aeea60d170f941fe901c6842f5df72e6
Gabriel dos Reis <bielzireiscosta.com@gmail.com>
2025-05-12 19:53:11 -0300

    criação classe cliente

---
+++ models/cliente.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+import datetime
+
+
+@dataclass
+class Cliente:
+    id: int
+    nome: str
+    cpf: str
+    telefone: str
+    email: str
+    data_nascimento: datetime
+
