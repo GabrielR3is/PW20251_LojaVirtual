0ac809d1d715d294e7936cfb9e4020d45270a4cf
Gabriel dos Reis <bielzireiscosta.com@gmail.com>
2025-05-19 19:01:49 -0300

    mudança import datetime clienterepo

---
+++ data/insert_clientes.sql
@@ -0,0 +1,13 @@
+-- comando inser parainserir 10 clientes na tabela clientes contendo nome, cpf, email, telefone e data_nascimento -- 
+INSERT INTO clientes (nome, cpf, email, telefone, data_nascimento) VALUES
+('João Silva', '12345678901', '123.456.789-00', 'joaosilva@gmail.com', '123456789'),
+('Maria Oliveira', '23456789012', '234.567.890-11', 'Mariaoliveira@gmail.com','234567890'),
+('Carlos Souza', '34567890123', 'carlos.souza@gmail.com', '345678901', '1990-03-15'),
+('Ana Paula', '45678901234', 'ana.paula@gmail.com', '456789012', '1988-07-22'),
+('Pedro Santos', '56789012345', 'pedro.santos@gmail.com', '567890123', '1992-11-30'),
+('Fernanda Lima', '67890123456', 'fernanda.lima@gmail.com', '678901234', '1985-05-18'),
+('Lucas Pereira', '78901234567', 'lucas.pereira@gmail.com', '789012345', '1995-09-10'),
+('Juliana Costa', '89012345678', 'juliana.costa@gmail.com', '890123456', '1991-12-05'),
+('Rafael Alves', '90123456789', 'rafael.alves@gmail.com', '901234567', '1987-02-27'),
+('Patrícia Mendes', '01234567890', 'patricia.mendes@gmail.com', '012345678', '1993-08-14'),
+('Bruno Rocha', '10987654321', 'bruno.rocha@gmail.com', '109876543', '1989-04-23');
